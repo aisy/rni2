10ee3be7bb5be99676164daa8a2fe8c9e5c91194
aisy <aisymuhammad@gmail.com>
2021-01-05 07:44:53 +0700

    add styling

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native' //memanggil stylesheet dari React native
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      // menerapkan style dari varibel styles dengan object stylenya
+      <View style={styles.bgColor}>
+        <Text style={styles.textColor}>
+          Hello, world!
+        </Text>
       </View>
-    )
+    );
   }
 }
+
+// pembuatan variabel styles dengan beberapa object style
+const styles = StyleSheet.create({
+  bgColor: {
+    backgroundColor: "red",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textColor: {
+    color: "white",
+    fontSize: 20
+  }
+});
